a57be53f1b218ba7f4b707f8c66a01a089b2118b
Earthlinedevelopment <info@earthlinedevelopment.org>
2026-06-05 08:46:08 -0400

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Earthline</h1>
+
+      <h2>Aquifer Recharge Starts Here</h2>
+
+      <p>
+        How can this landscape recharge more water,
+        retain more soil, and become more resilient?
+      </p>
+    </main>
+  );
+}
